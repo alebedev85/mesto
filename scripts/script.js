eb10d35638580ea349e92bad598cb42f65fd0f36
Andrey Lebedev <alebedev424@gmail.com>
2022-12-08 15:46:04 +0300

    feat: добавил редактирование профиля через форму

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,34 @@
-let editButton = document.querySelector('.edit-button')
-let popup = document.querySelector('.popup')
-let closeButton = document.querySelector('.popup__close-button')
+let editButton = document.querySelector('.edit-button');
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__close-button');
+let profileName = document.querySelector('.profile__name').textContent;
+let profileJob = document.querySelector('.profile__job').textContent;
+
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('#user-name');
+let jobInput = document.querySelector('#user-job');
 
 
-editButton.addEventListener('click', function () {popup.classList.toggle('popup_opened')})
-closeButton.addEventListener('click', function () {popup.classList.toggle('popup_opened')})
+function openForm() {
+    popup.classList.toggle('popup_opened');
+    nameInput.value = profileName;
+    jobInput.value = profileJob;
+};
+
+function closeForm() {
+    popup.classList.toggle('popup_opened');
+};
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+
+    document.querySelector('.profile__name').textContent = nameInput.value;
+    profileJob = document.querySelector('.profile__job').textContent = jobInput.value;
+    closeForm();
+};
+
+formElement.addEventListener('submit', handleFormSubmit);
+
+
+editButton.addEventListener('click', openForm);
+closeButton.addEventListener('click', closeForm);
