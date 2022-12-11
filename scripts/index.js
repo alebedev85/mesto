5a535f80784caf154b0e5d5afa25c2b43b066078
Lebedev Andrey <alebedev424@gmail.com>
2022-12-11 12:44:37 +0300

    fix: подправил index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,29 +2,29 @@ let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close-button');
 
+let profileName = document.querySelector('.profile__name').textContent;
+let profileJob = document.querySelector('.profile__job').textContent;
+
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('#user-name');
 let jobInput = document.querySelector('#user-job');
 
 
 function openForm() {
-    let profileName = document.querySelector('.profile__name').textContent;
-    let profileJob = document.querySelector('.profile__job').textContent;
-
-    popup.classList.toggle('popup_opened');
+    popup.classList.add('popup_opened');
     nameInput.value = profileName;
     jobInput.value = profileJob;
 };
 
 function closeForm() {
-    popup.classList.toggle('popup_opened');
+    popup.classList.remove('popup_opened');
 };
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
 
     document.querySelector('.profile__name').textContent = nameInput.value;
-    profileJob = document.querySelector('.profile__job').textContent = jobInput.value;
+    document.querySelector('.profile__job').textContent = jobInput.value;
     closeForm();
 };
 
