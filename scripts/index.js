25aa10b9db406f295d1d069c29d330549c30fd57
Andrey Lebedev <alebedev424@gmail.com>
2022-12-23 12:00:19 +0300

    feat: добавил add-popup, сделал функции уневерсальными, добавил плавность открытия

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,57 @@
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+//Buttons//
 const editButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
+const addButton = document.querySelector('.profile__add-button');
 const closeButton = document.querySelector('.popup__close-button');
 
+//Edit Popup//
+const editPopup = document.querySelector('.popup_type_edit');
+const closeEditButton = editPopup.querySelector('.popup__close-button');
+const formElement = editPopup.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.popup__input_input_name');
+const jobInput = formElement.querySelector('.popup__input_input_job');
+
+//Edd Popup//
+const addPopup = document.querySelector('.popup_type_add');
+const closeEddButton = addPopup.querySelector('.popup__close-button');
+
+//profile//
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 
-const formElement = document.querySelector('.popup__form');
-const nameInput = document.querySelector('.popup__input_input_name');
-const jobInput = document.querySelector('.popup__input_input_job');
-
-
-function openForm() {
+function openPoup(popup) {
     popup.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 };
 
-function closeForm() {
+function closeForm(popup) {
     popup.classList.remove('popup_opened');
 };
 
@@ -31,5 +66,8 @@ function handleFormSubmit(evt) {
 formElement.addEventListener('submit', handleFormSubmit);
 
 
-editButton.addEventListener('click', openForm);
-closeButton.addEventListener('click', closeForm);
+editButton.addEventListener('click', () => {openPoup(editPopup)});
+closeEditButton.addEventListener('click', () => {closeForm(editPopup)});
+
+addButton.addEventListener('click', () => {openPoup(addPopup)});
+closeEddButton.addEventListener('click', () => {closeForm(addPopup)});
