2c6e07506acf62318b9fbc4de3d2cc53dc7cb6c0
Lebedev Andrey <alebedev424@gmail.com>
2023-01-11 20:38:23 +0300

    feater: добавил управление кнопкой

--- scripts/validate.js
+++ scripts/validate.js
@@ -2,13 +2,13 @@ const showInputError = (formElement, inputElement, errorMessage) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.add('popup__input-outline-red');
   errorElement.textContent = errorMessage;
-  errorElement.classList.add('popup__input-error_active');
+  // errorElement.classList.add('popup__input-error_active');
 };
 
 const hideInputError = (formElement, inputElement) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.remove('popup__input-outline-red');
-  errorElement.classList.remove('popup__input-error_active');
+  // errorElement.classList.remove('popup__input-error_active');
   errorElement.textContent = '';
 };
 
@@ -20,11 +20,28 @@ const checkInputValidity = (formElement, inputElement) => {
   }
 };
 
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+}
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('button_inactive');
+  } else {
+    buttonElement.classList.remove('button_inactive');
+  }
+}
+
 const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__save-button');
+  toggleButtonState(inputList, buttonElement);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
       checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
     });
   });
 };
