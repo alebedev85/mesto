8b1de37b5a3da5565dd3cedb84780e712a3b346a
Lebedev Andrey <alebedev424@gmail.com>
2023-01-14 13:19:35 +0300

    refact: перенес вызов всех функций в конец файла

--- scripts/index.js
+++ scripts/index.js
@@ -78,10 +78,8 @@ function creatCardsFromArray() {
   initialCards.forEach((item) => renderCard(item.name, item.link))
 }
 
-creatCardsFromArray()
-
 //Close Popup with Esc//
-function closePopupWithEsc (evt) {
+function closePopupWithEsc(evt) {
   if (evt.key === 'Escape') {
     const activePopup = document.querySelector('.popup_opened');
     closePopup(activePopup);
@@ -111,37 +109,54 @@ function closePopup(popup, evt) {
   document.removeEventListener('click', closePopupWithOverlay);
 };
 
-//Open and Close Edit Popup//
-buttonEditProfile.addEventListener('click', () => {
-  openPopup(popupEditProfile);
-  profileNameInput.value = profileName.textContent;
-  profileJobInput.value = profileJob.textContent;
-});
-buttonClosePopupEditProfile.addEventListener('click', () => closePopup(popupEditProfile));
-
-//Open and Close Add Popup//
-buttonAddNewCard.addEventListener('click', () => openPopup(popupAddCard));
-buttonClosePopupAddCard.addEventListener('click', () => closePopup(popupAddCard));
-
-//Close Edit Picture//
-buttonClosePicturePopup.addEventListener('click', () => closePopup(popupPicture));
-
 //Handle Edit Form//
 function handleEditFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = profileNameInput.value;
   profileJob.textContent = profileJobInput.value;
 };
-formEditProfile.addEventListener('submit', (evt) => {
-  handleEditFormSubmit(evt);
-  closePopup(popupEditProfile)});
 
 //Handle Add Form//
 function handleAddFormSubmit(evt) {
   evt.preventDefault();
   renderCard(cardNameImput.value, cardLinkImput.value);
 };
+
+//Add Event Listeners//
+
+//Open Edit Popup//
+buttonEditProfile.addEventListener('click', () => {
+  openPopup(popupEditProfile);
+  profileNameInput.value = profileName.textContent;
+  profileJobInput.value = profileJob.textContent;
+});
+
+//Close Edit Popup//
+buttonClosePopupEditProfile.addEventListener('click', () => closePopup(popupEditProfile));
+
+//Open Add Popup//
+buttonAddNewCard.addEventListener('click', () => openPopup(popupAddCard));
+
+//Close Add Popup//
+buttonClosePopupAddCard.addEventListener('click', () => closePopup(popupAddCard));
+
+//Close Edit Picture//
+buttonClosePicturePopup.addEventListener('click', () => closePopup(popupPicture));
+
+//Submit Edit Profile//
+formEditProfile.addEventListener('submit', (evt) => {
+  handleEditFormSubmit(evt);
+  closePopup(popupEditProfile);
+});
+
+//Submit Edd Card//
 formEddCard.addEventListener('submit', (evt) => {
   handleAddFormSubmit(evt);
   closePopup(popupAddCard);
-  evt.target.reset()});
+  evt.target.reset();
+});
+
+//Main Funtions//
+
+//Creat Cards From Array//
+creatCardsFromArray();
