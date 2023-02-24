ac27448cf2ef232ce5f40988dd5a6ec57355c35f
Lebedev Andrey <alebedev424@gmail.com>
2023-02-24 21:26:31 +0300

    refactor: перенес все константы в новый фаил

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 // import './pages/index.css';
-import { popupPicture, imagePicturPopup, titlePicturPopup } from './scripts/constants.js';
+import {
+  buttonEditProfile,
+  buttonAddNewCard,
+  buttonClosePopupEditProfile,
+  buttonClosePopupAddCard,
+  buttonClosePicturePopup,
+  profileNameInput,
+  profileJobInput,
+  profileName,
+  profileJob
+} from './scripts/constants.js';
 import initialCards from './scripts/cards.js';
 import selectors from './scripts/selectors.js';
 import FormValidator from './components/FormValidator.js';
@@ -12,35 +22,37 @@ import UserInfo from './components/UserInfo.js';
 //VARS//
 const formsCollection = {};
 
-//Cards//
-// const cardsContainer = document.querySelector('.elements')
+//Creat Cards From Array//
+const CardsSection = new Section({
+  items: initialCards,
+  renderer: ({ name, link }) => {
+    const card = new Card(name, link, '.element-temlate', () => {
+      popupWithImage.open(name, link);
+    });
+    return card.creatCard();
+  }
+},
+  '.elements')
 
-//Main Buttons//
-const buttonEditProfile = document.querySelector('.profile__edit-button');
-const buttonAddNewCard = document.querySelector('.profile__add-button');
+const userInfo = new UserInfo({
+  selectorUserName: '.profile__name',
+  selectorUserInfo: '.profile__job'
+});
 
-//Edit Profile Popup//
-const popupEditProfile = document.querySelector('.popup_type_edit');
+///Form edit profile///
+//Creat element//
+const editProfile = new PopupWithForm('.popup_type_edit', (data) => {
+  userInfo.setUserInfo(data);
+})
 
-const buttonClosePopupEditProfile = popupEditProfile.querySelector('.popup__close-button');
-
-const formEditProfile = popupEditProfile.querySelector('.popup__form');
-const profileNameInput = formEditProfile.querySelector('.popup__input_input_name');
-const profileJobInput = formEditProfile.querySelector('.popup__input_input_job');
-
-//Edd Card Popup//
-const popupAddCardOld = document.querySelector('.popup_type_add');
-const buttonClosePopupAddCard = popupAddCardOld.querySelector('.popup__close-button');
-const formEddCard = popupAddCardOld.querySelector('.popup__form');
-const cardNameImput = formEddCard.querySelector('.popup__input_input_place');
-const cardLinkImput = formEddCard.querySelector('.popup__input_input_link');
-
-//Picture popup//
-const buttonClosePicturePopup = popupPicture.querySelector('.popup__close-button');
-
-//Profile//
-const profileName = document.querySelector('.profile__name');
-const profileJob = document.querySelector('.profile__job');
+///Form add new card///
+//Creat element//
+const addCard = new PopupWithForm('.popup_type_add', ({ cardLinkImput: link, cardNameImput: name }) => {
+  const newCard = new Card(name, link, '.element-temlate', () => {
+    popupWithImage.open(name, link);
+  });
+  CardsSection.addItem(newCard.creatCard());
+})
 
 //FUNCTIONS//
 
@@ -53,34 +65,16 @@ function enableValidation({ formSelector, ...rest }) {
   });
 };
 
-//Main Funtions//
+///Popup With Image///
+//Creat element//
+const popupWithImage = new PopupWithImage('.popup_type_picture')
 
-//Creat Cards From Array//
-const CardsSection = new Section({
-  items: initialCards,
-  renderer: ({ name, link }) => {
-    const card = new Card(name, link, '.element-temlate', () => {
-      popupWithImage.open(name, link);
-    });
-    return card.creatCard();
-  }
-},
-  '.elements')
-CardsSection.rendererElements()
+//Main Funtions//
 
 //Launch Form Validation//
 enableValidation(selectors)
 
-
-const userInfo = new UserInfo({
-  selectorUserName: '.profile__name',
-  selectorUserInfo: '.profile__job'});
-
-///Form edit profile///
-//Creat element//
-const editProfile = new PopupWithForm('.popup_type_edit', (data) => {
-  userInfo.setUserInfo(data);
-})
+CardsSection.rendererElements()
 
 //Set listener for open edit form//
 buttonEditProfile.addEventListener('click', () => {
@@ -93,16 +87,6 @@ buttonEditProfile.addEventListener('click', () => {
 //Set listener for close edit form//
 buttonClosePopupEditProfile.addEventListener('click', () => editProfile.close());
 
-
-///Form add new card///
-//Creat element//
-const addCard = new PopupWithForm('.popup_type_add', ({ cardLinkImput: link, cardNameImput: name }) => {
-  const newCard = new Card(name, link, '.element-temlate', () => {
-    popupWithImage.open(name, link);
-  });
-  CardsSection.addItem(newCard.creatCard());
-})
-
 //Set listener for open add new card form//
 buttonAddNewCard.addEventListener('click', () => {
   formsCollection['formAddCard'].resetInputError();
@@ -111,10 +95,6 @@ buttonAddNewCard.addEventListener('click', () => {
 
 //Set listener for close edit form//
 buttonClosePopupAddCard.addEventListener('click', () => addCard.close());
-
-///Popup With Image///
-//Creat element//
-const popupWithImage = new PopupWithImage('.popup_type_picture')
 
 //Set listener for close edit form//
 buttonClosePicturePopup.addEventListener('click', () => popupWithImage.close());
--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -2,3 +2,27 @@
 export const popupPicture = document.querySelector('.popup_type_picture');
 export const imagePicturPopup = popupPicture.querySelector('.popup__picture');
 export const titlePicturPopup = popupPicture.querySelector('.popup__picture-title');
+
+//Main Buttons//
+export const buttonEditProfile = document.querySelector('.profile__edit-button');
+export const buttonAddNewCard = document.querySelector('.profile__add-button');
+
+//Edit Profile Popup//
+export const popupEditProfile = document.querySelector('.popup_type_edit');
+
+export const buttonClosePopupEditProfile = popupEditProfile.querySelector('.popup__close-button');
+
+export const formEditProfile = popupEditProfile.querySelector('.popup__form');
+export const profileNameInput = formEditProfile.querySelector('.popup__input_input_name');
+export const profileJobInput = formEditProfile.querySelector('.popup__input_input_job');
+
+//Edd Card Popup//
+export const popupAddCardOld = document.querySelector('.popup_type_add');
+export const buttonClosePopupAddCard = popupAddCardOld.querySelector('.popup__close-button');
+
+//Picture popup//
+export const buttonClosePicturePopup = popupPicture.querySelector('.popup__close-button');
+
+//Profile//
+export const profileName = document.querySelector('.profile__name');
+export const profileJob = document.querySelector('.profile__job');
