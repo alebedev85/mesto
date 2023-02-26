81bac8c816d2351c54076c88d9288f2847627c35
Lebedev Andrey <alebedev424@gmail.com>
2023-02-26 12:13:51 +0300

    fix: переделал close в PWF

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -28,8 +28,13 @@ export default class PopupWithForm extends Popup {
       evt.preventDefault();
       this._handleFormSubmit(this._getInputValues());
       evt.target.reset();
-      super.close();
+      this.close();
     });
     super._setEventListeners();
+  }
+
+  close() {
+    this._formElement.reset();
+    super.close()
   }
 }
